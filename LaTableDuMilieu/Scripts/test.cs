9731736c2b8093495f8408267a07ef8d9ed99751
matheoleger <71370253+matheoleger@users.noreply.github.com>
2020-11-03 19:08:26 +0100

    GROS COMMIT CA MERE

---
+++ LaTableDuMilieu/Scripts/test.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class test : Node
+{
+    public override void _Ready()
+    {
+        GD.Print("coucou c'est wam frere ET OUAI MEC EN C# sur GODOT ;)");
+    }
+
+}
