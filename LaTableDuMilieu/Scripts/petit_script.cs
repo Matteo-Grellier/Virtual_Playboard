98806f1bfab5db9b28a2e3ae66feeb3c0e60cbdd
matheoleger <71370253+matheoleger@users.noreply.github.com>
2020-11-04 11:41:36 +0100

    commit de petit test

---
+++ LaTableDuMilieu/Scripts/petit_script.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class petit_script : Node
+{
+    // Declare member variables here. Examples:
+    //public int a;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("Si vous voulez, venez voir ce que je fais sur YouTube");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//
+//  }
+}
