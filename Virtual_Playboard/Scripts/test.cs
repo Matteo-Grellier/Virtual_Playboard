d7c0b80ab54cef93f71df4aaa3a23376cbc08778
Matteo Grellier <46086160+Matteo-Grellier@users.noreply.github.com>
2020-12-09 09:58:04 +0100

    Mise à jour du projet, permet de déplacer une image avec les flèches directionnelle

--- Virtual_Playboard/Scripts/test.cs
+++ Virtual_Playboard/Scripts/test.cs
@@ -1,21 +1,34 @@
 using Godot;
 using System;
 
-public class test : Node
+public class test : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+    [Export] public int speed = 200;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("Helloo from the other side et ouai mec ;)");
-	}
+    public Vector2 velocity = new Vector2();
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//
-//  }
+    public void GetInput()
+    {
+        velocity = new Vector2();
+
+        if (Input.IsActionPressed("right"))
+            velocity.x += 1;
+
+        if (Input.IsActionPressed("left"))
+            velocity.x -= 1;
+
+        if (Input.IsActionPressed("down"))
+            velocity.y += 1;
+
+        if (Input.IsActionPressed("up"))
+            velocity.y -= 1;
+
+        velocity = velocity.Normalized() * speed;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        velocity = MoveAndSlide(velocity);
+    }
 }
