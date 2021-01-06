a9cb9410e528b8deeb847cd0fc2bd956073c3c52
Matteo Grellier <46086160+Matteo-Grellier@users.noreply.github.com>
2021-01-06 19:04:08 +0100

    bug

---
+++ Scripts/pieces.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public class pieces : KinematicBody2D
+{
+
+    public float[] rightCoor = new float[2];
+
+    //pour _Input()
+    public Vector2 mouseClick = new Vector2();
+    public float mouseX;
+    public float mouseY;
+
+    //pour Movement()
+    public int speed = 400;
+    public Vector2 toMovement = new Vector2();
+    public Vector2 velocity = new Vector2();
+
+
+    //position de la souris
+    public override void _Input(InputEvent @event)
+    {
+        board Board = new board();
+        // rightCoor = test.RightCoor(mouseClick.x, mouseClick.y);
+
+        //Mouse in viewport coordinates.
+        if (@event is InputEventMouseButton eventMouseButton)
+        {
+            GD.Print("Mouse Click/Unclick at: ", eventMouseButton.Position);
+            mouseClick = eventMouseButton.Position;
+
+            //Debug
+            GD.Print(mouseClick.x);
+            GD.Print(mouseClick.y);
+
+            rightCoor = Board.RightCoor(mouseClick.x, mouseClick.y);
+
+            //Debug
+            GD.Print(rightCoor[0]);
+        }  
+
+        toMovement.x = rightCoor[0];
+        toMovement.y = rightCoor[1];
+        //Movement();
+        
+    }
+    
+
+    public override void _PhysicsProcess(float delta)
+    {
+        velocity = Position.DirectionTo(toMovement) * speed;
+
+        if (Position.DistanceTo(toMovement) > 5)
+        {
+            velocity = MoveAndSlide(velocity);
+        }
+    }
+
+}
