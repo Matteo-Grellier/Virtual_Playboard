cacc0c9991170e749caa2bebc476446b8adbf2ed
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-01-11 15:21:20 +0100

    Update

--- Scripts/pieces.cs
+++ Scripts/pieces.cs
@@ -15,12 +15,12 @@ public class pieces : KinematicBody2D
     public int speed = 400;
     public Vector2 toMovement = new Vector2();
     public Vector2 velocity = new Vector2();
-
+    float t;
 
     //position de la souris
     public override void _Input(InputEvent @event)
     {
-        board Board = new board();
+        board Board = new board(); //référence au code board.cs (je crois)
         // rightCoor = test.RightCoor(mouseClick.x, mouseClick.y);
 
         //Mouse in viewport coordinates.
@@ -48,12 +48,22 @@ public class pieces : KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
+        //Node child = GetChild(0);
+
+        //Node child = GetNode("KinematicBody2D");
+
+        //Node2D obj = GetNode("KinematicBody2D").Object; // Property access.
+
+        //t += delta * 0.4f;
+
         velocity = Position.DirectionTo(toMovement) * speed;
 
-        if (Position.DistanceTo(toMovement) > 5)
-        {
-            velocity = MoveAndSlide(velocity);
-        }
+        // if (Position.DistanceTo(toMovement) > 0)
+        // {
+        velocity = MoveAndSlide(velocity);
+        // }
+
+        //obj.Position = mouseClick.LinearInterpolate(toMovement, t);
     }
 
 }
