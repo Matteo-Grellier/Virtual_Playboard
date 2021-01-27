fcfad98d3f37fb870f11695468e61b06dba35a3d
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-01-27 13:30:31 +0100

    Update (add rook.cs)

--- Scripts/rook.cs
+++ Scripts/rook.cs
@@ -9,37 +9,130 @@ public class rook : Node
 
     // Called when the node enters the scene tree for the first time.
 
-    public float bFirstRookX = 32;
+    // Les valeurs sont dans l'ordre : 1ere valeur = X et Y du rook blanc gauche (a1) / 2eme = X et Y du rook blanc droit (h1)
+    // 3eme = X et Y du rook noir gauche (a8) / 4eme = X et Y du rook noir droit (h8)
+    public float[] rookX = new float[4];   
+    public float[] rookY = new float[4];
+
+    public Vector2 testVec = new Vector2();
+
+    //public float test;
 
     public override void _Ready()
     {
-        
+    //     Position2D.GlobalPosition = new Vector2();
+
+        // var test = Get("Node2D/position");
+        // GD.Print(test);
+
+        testVec = rook.GlobalPosition;
     }
+
+    // public override void _PhysicsProcess(float delta)
+    // {
+    //     GetGlobalPosition 
+    // }
+
+
+
+
 
     // public void _on_Area2D_input_event() {
 
     // }
 
-    public void _OnArea2DInputEvent(Viewport viewport, InputEvent @event, int shape_idx)
-    {
+    // public void _OnArea2DInputEvent(Viewport viewport, InputEvent @event, int shape_idx)
+    // {
 
-        // GD.Print("Clicked");
+    //     // GD.Print("Clicked");
 
-        // if (@event is InputEventMouseButton click && click.ButtonIndex == (int)ButtonList.Left && @event.IsPressed()) {
-        //     GD.Print("Clicked");
-        // }
+    //     // if (@event is InputEventMouseButton click && click.ButtonIndex == (int)ButtonList.Left && @event.IsPressed()) {
+    //     //     GD.Print("Clicked");
+    //     // }
 
 
-        if (@event is InputEventMouseButton) {
+    //     if (@event is InputEventMouseButton) {
+
+    //         GD.Print("Clicked");
+
+    //         if (@event.IsActionPressed("click")) {
+    //                 GD.Print("Clicked");
+    //         }
+    //     }
+
+    // }
+
+
+    // public void _OnArea2DInputEvent(Node n, InputEvent @event, int idx) { 
+
+    //     //if (@event is InputEventMouseButton) {
+
+    //         if (@event.IsActionPressed("click")) {
+    //                 GD.Print("Clicked");
+    //         }
+    //     //}
+
+    // }
+
+
+    // public void _on_Area2D_input_event() {
+
+    // }
+
+    // private void _OnSpriteInputEvent(Viewport viewport, InputEvent @event, int shape_idx)
+    // {
+
+    //     // GD.Print("Clicked");
+
+    //     // if (@event is InputEventMouseButton click && click.ButtonIndex == (int)ButtonList.Left && @event.IsPressed()) {
+    //     //     GD.Print("Clicked");
+    //     // }
+
+
+    //     if (@event is InputEventMouseButton) {
+
+    //         if (@event.IsActionPressed("click")) {
+    //                 GD.Print("Clicked");
+    //         }
+    //     }
+
+    // }
+
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    
+
+
+
+
+
+    // public void _on_Area2D_input_event() {
+
+    // }
+
+    // public void _OnArea2DInputEvent(Viewport viewport, InputEvent @event, int shape_idx)
+    // {
+
+    //     // GD.Print("Clicked");
+
+    //     // if (@event is InputEventMouseButton click && click.ButtonIndex == (int)ButtonList.Left && @event.IsPressed()) {
+    //     //     GD.Print("Clicked");
+    //     // }
+
+
+    //     if (@event is InputEventMouseButton) {
             
-            GD.Print("Clicked");
+    //         GD.Print("Clicked");
             
-            if (@event.IsActionPressed("click")) {
-                    GD.Print("Clicked");
-            }
-        }
+    //         if (@event.IsActionPressed("click")) {
+    //                 GD.Print("Clicked");
+    //         }
+    //     }
        
-    }
+    // }
 
 
     // public void _OnArea2DInputEvent(Node n, InputEvent @event, int idx) { 
