270dc7d143d4ade420bb8a760d2c3aff173d24c3
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2021-04-21 12:11:28 +0200

    fonctionnal menu

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+
+    RectTransform rectTransform;
+    void Start () 
+    {
+     rectTransform = GetComponent<RectTransform>();
+    }
+
     public void playGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -12,6 +19,26 @@ public class mainMenu : MonoBehaviour
 
     public void quitGame()
     {
+        print("Quit !");
         Application.Quit();
+    }
+
+    public void MenueLeft()
+    {
+        Vector2 aPos = rectTransform.position;
+        aPos.x = aPos.x * Time.deltaTime;
+        rectTransform.position = aPos;
+    }
+
+    public void MenueRight()
+    {
+        Vector2 aPos = rectTransform.position;
+        aPos.x = aPos.x * Time.deltaTime;
+        rectTransform.position = aPos;
+    }
+
+    public void Settings()
+    {
+        
     }
 }
